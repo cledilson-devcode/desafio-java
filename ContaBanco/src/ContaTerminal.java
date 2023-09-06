b28af592aef84383e55ab974f3d9e66ba1a4fe23
cledilson <cledilsondevoffice@gmail.com>
2023-09-06 17:18:55 -0300

    feat: Adicionado os pré-requsitos da entrega do desafio. Porem acrescentei um looping para a confirmação dos dados do cliente.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner scan = new Scanner(System.in);
+
+        String confirmar = null;
+
+        String nomeCliente = null;
+        String agencia = null;
+        int numeroConta = 0;
+        double saldo = 0.0;
+
+
+        while (!"s".equalsIgnoreCase(confirmar) ){
+            System.out.println("Digite o seu nome: ");
+            nomeCliente = scan.next();
+            System.out.println("Digite sua agencia: ");
+            agencia = scan.next();
+            System.out.println("Digite o número da conta: ");
+            numeroConta = scan.nextInt();
+            System.out.println("Digite seu saldo: ");
+            saldo = scan.nextDouble();
+
+            System.out.println("Confirme se os seus dados estão corretos");
+            System.out.println("'s' para sim / 'n' para não");
+            confirmar = scan.next();
+
+        }
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numeroConta +" e seu saldo R$"+ saldo+" já está disponível para saque");
+
+
+
+
     }
 
 }
